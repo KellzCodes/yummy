6ffd21b1054373bc185f4949f258d2ebfd31e3dc
Kelli <keldavis5@yahoo.com>
2018-12-18 14:11:46 -0500

    refactor step adapter

--- app/src/main/java/com/kelldavis/yummy/adapter/StepAdapter.java
+++ app/src/main/java/com/kelldavis/yummy/adapter/StepAdapter.java
@@ -16,94 +16,79 @@
 
 package com.kelldavis.yummy.adapter;
 
-import android.databinding.DataBindingUtil;
-import android.support.annotation.NonNull;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.kelldavis.yummy.R;
-import com.kelldavis.yummy.databinding.StepListItemBinding;
 import com.kelldavis.yummy.model.Step;
-import com.kelldavis.yummy.utilities.OnItemClickListener;
 
-import java.util.ArrayList;
+public class StepAdapter extends RecyclerView.Adapter<StepAdapter.StepAdapterViewHolder> {
+    private static final String TAG = StepAdapter.class.getSimpleName();
+    final private StepAdapter.StepAdapterOnClickHandler mClickHandler;
+    private Step[] mRecipeSteps;
 
-public class StepAdapter extends RecyclerView.Adapter<StepAdapter.StepHolder> {
-    private ArrayList<Step> mStepList;
-    private OnItemClickListener mStepOnItemClickListener;
-    private int mSelectedPosition = 0;
-
-    public StepAdapter(ArrayList<Step> incomingStepSet,
-                       OnItemClickListener<Step> StepOnItemClickListener) {
-        this.mStepList = incomingStepSet;
-        this.mStepOnItemClickListener = StepOnItemClickListener;
-    }
-
-    @NonNull
-    @Override
-    public StepHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        StepListItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.step_list_item, parent, false);
-        return new StepHolder(binding);
+    public StepAdapter(StepAdapter.StepAdapterOnClickHandler stepAdapterOnClickHandler) {
+        mClickHandler = stepAdapterOnClickHandler;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StepHolder holder, int position) {
-        holder.mStep = mStepList.get(position);
-        holder.mBinding.cvStepItemHolder.setSelected(mSelectedPosition == position);
+    public StepAdapter.StepAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.item_step_card;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
 
-        if(holder.mStep.getVideoURL()!=null && !holder.mStep.getVideoURL().matches("")) {
-            Glide.with(holder.itemView.getContext())
-                    .load(holder.mStep.getThumbnailURL())
-                    .apply(new RequestOptions()
-                            .placeholder(R.drawable.ic_videocam_black_24dp)
-                            .error(R.drawable.ic_videocam_black_24dp)
-                            .dontAnimate()
-                            .fitCenter())
-                    .into(holder.mBinding.ivStepItemVideoThumb);
-        } else {
-            holder.mBinding.ivStepItemVideoThumb.setImageResource(R.drawable.ic_videocam_off_black_24dp);
-        }
-        if(holder.mStep.getId() == 0)
-            holder.mBinding.tvStepListStepNumber.setText("   ");
-        else
-            holder.mBinding.tvStepListStepNumber.setText(String.valueOf(holder.mStep.getId()) + ": ");
-        holder.mBinding.tvStepListStepShortDesc.setText(holder.mStep.getShortDescription());
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        StepAdapter.StepAdapterViewHolder viewHolder = new StepAdapter.StepAdapterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(StepAdapter.StepAdapterViewHolder holder, int position) {
+        int recipeStepNumber = mRecipeSteps[position].getId();
+        // the recipe introduction gets grouped with the steps but doesn't have a step number in its description
+        String stepNumberString = recipeStepNumber > 0 ? String.format("Step %d: ", recipeStepNumber) : "";
+        String recipeShortDescription = mRecipeSteps[position].getShortDescription();
+
+        holder.tvStepNumberAndShortDescription.setText(String.format(stepNumberString + recipeShortDescription));
     }
 
     @Override
     public int getItemCount() {
-        return (mStepList == null) ? 0 : mStepList.size();
+        if (mRecipeSteps == null) {
+            return 0;
+        } else {
+            return mRecipeSteps.length;
+        }
     }
 
-    public class StepHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private final StepListItemBinding mBinding;
-        private Step mStep;
+    public void setRecipeStepData(Step[] recipeSteps) {
+        mRecipeSteps = recipeSteps;
+        notifyDataSetChanged();
+    }
 
-        public StepHolder(StepListItemBinding binding) {
-            super(binding.getRoot());
-            this.mBinding = binding;
+    public interface StepAdapterOnClickHandler {
+        void onClick(int whichStep);
+    }
 
-            binding.cvStepItemHolder.setOnClickListener(this);
+    public class StepAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final TextView tvStepNumberAndShortDescription;
+
+        public StepAdapterViewHolder(View itemView) {
+            super(itemView);
+            tvStepNumberAndShortDescription = itemView.findViewById(R.id.tv_step_number_and_short_description);
+            itemView.setOnClickListener(this);
         }
 
         @Override
-        public void onClick(View v) {
-            notifyItemChanged(mSelectedPosition);
-            mSelectedPosition = getLayoutPosition();
-            notifyItemChanged(mSelectedPosition);
-            mStepOnItemClickListener.onClick(mStep, v);
+        public void onClick(View view) {
+            int whichStep = getAdapterPosition();
+            mClickHandler.onClick(whichStep);
         }
     }
-    public int getStepAdapterCurrentPosition(){
-        return mSelectedPosition;
-    }
-    public void setStepAdapterCurrentPosition(int savedPosition){
-        mSelectedPosition = savedPosition;
-    }
 }
-
