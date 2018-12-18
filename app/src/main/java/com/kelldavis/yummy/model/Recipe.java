d0cb2f53eeece6b202babc881afd65a138deb772
Kelli <keldavis5@yahoo.com>
2018-12-18 12:50:45 -0500

    refactor recipe model

--- app/src/main/java/com/kelldavis/yummy/model/Recipe.java
+++ app/src/main/java/com/kelldavis/yummy/model/Recipe.java
@@ -19,68 +19,10 @@ package com.kelldavis.yummy.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Recipe implements Parcelable {
-    @SerializedName("id")
-    @Expose
-    private Integer id;
-    @SerializedName("name")
-    @Expose
-    private String name;
-    @SerializedName("ingredients")
-    @Expose
-    private List<Ingredient> ingredients;
-    @SerializedName("steps")
-    @Expose
-    private List<Step> steps;
-    @SerializedName("servings")
-    @Expose
-    private Integer servings;
-    @SerializedName("image")
-    @Expose
-    private String image;
+    public static final Parcelable.Creator<Recipe> CREATOR
+            = new Parcelable.Creator<Recipe>() {
 
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public List<Step> getSteps() {
-        return steps;
-    }
-
-    public Integer getServings() {
-        return servings;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    protected Recipe(Parcel in) {
-        id = in.readInt();
-        name = in.readString();
-        servings = in.readInt();
-        image = in.readString();
-        ingredients = new ArrayList<>();
-        in.readTypedList(ingredients, Ingredient.CREATOR);
-        steps = new ArrayList<>();
-        in.readTypedList(steps, Step.CREATOR);
-    }
-
-    public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
         @Override
         public Recipe createFromParcel(Parcel in) {
             return new Recipe(in);
@@ -91,17 +33,124 @@ public class Recipe implements Parcelable {
             return new Recipe[size];
         }
     };
+    private int id;
+    private String name;
+    private Ingredient[] ingredients;
+    private Step[] steps;
+    private int servings;
+    private String imageUrl;
+    private String thumbnailUrl;
+    private String blurb;
 
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(id);
-        dest.writeString(name);
-        dest.writeInt(servings);
-        dest.writeString(image);
-        dest.writeTypedList(ingredients);
-        dest.writeTypedList(steps);
+
+    private Recipe(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        ingredients = in.createTypedArray(Ingredient.CREATOR);
+        steps = in.createTypedArray(Step.CREATOR);
+        servings = in.readInt();
+        imageUrl = in.readString();
+        thumbnailUrl = in.readString();
+        blurb = in.readString();
     }
 
+    public Recipe(int id, String name, Ingredient[] ingredients, Step[] steps, int servings, String imageUrl, String thumbnailUrl, String blurb) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.imageUrl = imageUrl;
+        this.thumbnailUrl = thumbnailUrl;
+        this.blurb = blurb;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getIngredientsCount() {
+        return ingredients.length;
+    }
+
+    public Step[] getSteps() {
+        return steps;
+    }
+
+    public void setSteps(Step[] steps) {
+        this.steps = steps;
+    }
+
+    public int getStepsCount() {
+        return steps.length;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getBlurb() {
+        return blurb;
+    }
+
+    public void setBlurb(String blurb) {
+        this.blurb = blurb;
+    }
+
+    @Override
     public int describeContents() {
         return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeTypedArray(ingredients, flags);
+        parcel.writeTypedArray(steps, flags);
+        parcel.writeInt(servings);
+        parcel.writeString(imageUrl);
+        parcel.writeString(thumbnailUrl);
+        parcel.writeString(blurb);
     }
 }
